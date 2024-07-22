0b511098259b615f312473ab54caf1ae8b7cecda
castberry10 <soc09072@gmail.com>
2024-07-22 17:30:07 +0900

    이미지 추가

--- mule2.py
+++ mule2.py
@@ -1,3 +1,8 @@
+# 랜덤 
+
+
+
+
 """
 this script is imgage processing 
 img directory -> get pwd and file 
@@ -30,53 +35,57 @@ def save_file(file_path, content):
         file.write(content)
 
 
-file_path = 'example.txt'
-new_file_path = 'example_modified.txt'
+# foreground_image_path = '/home/aicompetition28/wychoi/testDatasets/3.backsub_images_100/target.txt'
+# background_image_path = '/home/aicompetition28/wychoi/testDatasets/3.Background_Images/target.txt'
+# makelabeling_image_path = '/home/aicompetition28/colorfilteroneimage'
 
-# # 파일 읽기
-# content = read_file(file_path)
-# print("Original content:")
-# print(content)
+# foreground_image_list = read_file(foreground_image_path).split('\n')
+# background_image_list = read_file(background_image_path).split('\n')
 
-# # 수정된 내용 저장
-# save_file(new_file_path, content)
-# print(f"Modified content saved to {new_file_path}")
+# foreground_image_list_cnt = len(foreground_image_list)
+# background_image_list_cnt = len(background_image_list)
 
-path = "img"
-image_name = "20230823_185335_4.jpg"
-dir_path = os.getcwd()
+for i in range(30000):
+    # foreground_image_path = foreground_image_list[random.randint(0, foreground_image_list_cnt - 1)]
+    # background_image_path = background_image_list[random.randint(0, background_image_list_cnt - 1)]
+    
 
-
-
-image_path = os.path.join(dir_path, "AI", "img")
-print('image_path',  image_path)
-
-image_list: list = os.listdir(image_path)
-
-print(image_list)
+    ### test code
+    foreground_image_path = os.path.join(os.getcwd(), 'AI', 'img', 'campbells_chicken_noodle_soup_10000.jpg')
+    background_image_path = os.path.join(os.getcwd(), 'AI', 'img', '20230823_185335_4.jpg')
+    makelabeling_image_path = os.path.join(os.getcwd(), 'AI', 'img')
+    ### test code
 
 
-background_image = cv2.imread(os.path.join(image_path, image_list[3]), 1)  # background image
-class_image = cv2.imread(os.path.join(image_path, image_list[4]), 1)  # class image
+    background_image = cv2.imread(background_image_path)  # background image
+    class_image = cv2.imread(foreground_image_path)  # class image
 
-# class image processing
-color_range = (0, 40)
-red_filter = np.full_like(class_image, (random.randint(*color_range), random.randint(*color_range), random.randint(*color_range)))
+    # class image processing
+    color_range = (0, 40)
+    random_color_rgb = random.randint(*color_range), random.randint(*color_range), random.randint(*color_range)
+    rgb_str = str(random_color_rgb[0]) + '_' + str(random_color_rgb[1]) + '_' + str(random_color_rgb[2])
+    red_filter = np.full_like(class_image, random_color_rgb)
 
-mask = cv2.inRange(class_image, np.array([4, 4, 4]), np.array([255, 255, 255]))
-class_image = cv2.add(class_image, red_filter, mask=mask)
-background_image = cv2.bitwise_and(background_image, background_image, mask=cv2.bitwise_not(mask))
-# image = cv2.add(image, cv2.bitwise_or(class_image, class_image, mask=cv2.bitwise_not(mask)))
+    mask = cv2.inRange(class_image, np.array([4, 4, 4]), np.array([255, 255, 255]))
+    class_image = cv2.add(class_image, red_filter, mask=mask)
+    background_image = cv2.bitwise_and(background_image, background_image, mask=cv2.bitwise_not(mask))
+    image = cv2.add(background_image, class_image)
 
-image = cv2.add(background_image, class_image)
+    print(type(image))
+    cv2.imshow("image", image)
 
-print(type(image))
-cv2.imshow("image", image)
-# cv2.imshow("test", cv2.bitwise_and(class_image, class_image, mask=mask))
+    # # 이미지 저장
+    foreground_image_path_split = foreground_image_path.split(os.sep)
+    background_image_path_split = background_image_path.split(os.sep)
+    outputfilename = foreground_image_path_split[-1].split('.')[0] + '_' + background_image_path_split[-1].split('.')[0] + '_' + rgb_str + '.jpg'
+    makelabeling_image_file_path = os.path.join(makelabeling_image_path, outputfilename)
+    cv2.imwrite(makelabeling_image_file_path, image)
+    print(f"Image saved to {makelabeling_image_file_path}")
 
-# # 이미지 저장
-# output_path = os.path.join(image_path, 'output_image.jpg')
-# cv2.imwrite(output_path, image)
+    # 욜로 파일 생성
+    content = read_file(foreground_image_path[:-3]+'txt')
+    save_file(makelabeling_image_file_path[:-3]+'txt', content)
+    print(f"YOLO file saved to {makelabeling_image_file_path[:-3]+'txt'}")
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
